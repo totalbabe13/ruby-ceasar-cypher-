89b802e70bd85ec87c0ff441355b437a810ca9b0
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-12 15:40:24 -0500

    cypher method can accept spaces and special characters

--- ruby_cypher.rb
+++ ruby_cypher.rb
@@ -1,38 +1,69 @@
-# Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
-
-#   > caesar_cipher("What a string!", 5)
-#   => "Bmfy f xywnsl!"
-# Quick Tips:
-
-# You will need to remember how to convert a string into a number.
-# Don’t forget to wrap from z to a.
-# Don’t forget to keep the same case.
-
 def caesar_cipher(phrase,num)
   alphabet = ("a".."z").to_a
   numbers = (1..26).to_a
   
   #convert phrase to array
   phrase_array = phrase.split('')
+  puts '- - Phrase Array - -'
+  print phrase_array
+  puts ''
+  puts '- - - - - - - '
  
   #convert phrase_array to numbers 
   number_code = []
   x = 0
   while x != phrase_array.length
-    number_code << alphabet.index(phrase_array[x]) + num
-    x += 1
+    if alphabet.index(phrase_array[x])
+      number_code << alphabet.index(phrase_array[x]) + num
+      x += 1
+    else
+      number_code << phrase_array[x]
+      x += 1
+    end   
   end
+  puts ' '
+  puts ' - - Number Code - -'
+  print number_code
+  puts ''
+  puts '- - - - - - - '
 
   #use numbers with cypher to find new letter index
-  find_new_letter_index =  number_code.each {|item| item + num }
+  
+  find_new_letter_index = []
+  
+  number_code.map do |item| 
+    if item.is_a?(Integer)
+      find_new_letter_index << item + num
+    else
+      find_new_letter_index << item   
+    end  
 
-  # match ne letter index with letters
+  end
+  
+  puts ' '
+  puts ' - - Cypher Code - -'
+  print find_new_letter_index
+  puts ''
+  puts '- - - - - - - '
+
+  # match new letter index with letters
   secret_message_array = []
-  find_new_letter_index.each do |item|
-     secret_message_array << alphabet[item] 
-   end
 
-   puts secret_message_array.join
+  find_new_letter_index.each do |item|
+     if item.is_a?(Integer)
+       secret_message_array << alphabet[item]
+     else
+       secret_message_array << item
+     end     
+   end
+  
+  puts ' '
+  puts ' - - Coded Phrase - -'
+  print secret_message_array
+  puts ''
+  puts '- - - - - - - '
+  puts '- - Coded Message - - ' 
+  print "-->  " + secret_message_array.join
 end
 
-caesar_cipher('hello', 3)
+caesar_cipher('hello world!', 3)
